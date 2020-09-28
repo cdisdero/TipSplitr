087f67360b8f56fe989597dc75f766911d109bf9
Christopher Disdero <me@chrisdisdero.com>
2020-09-27 22:02:01 -0700

    Force sdk to 14.0

--- .github/scripts/archive_app.sh
+++ .github/scripts/archive_app.sh
@@ -4,6 +4,6 @@ set -eo pipefail
 
 xcodebuild -project TipSplitr.xcodeproj \
             -scheme TipSplitr \
-            -sdk iphoneos \
+            -sdk iphoneos14.0 \
             -archivePath $PWD/build/TipSplitr.xcarchive \
             clean archive | xcpretty
